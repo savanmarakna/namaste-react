4376e0fd25f5acf237e2dca50f3f3eaeb82cedfe
Savan Marakna <savan.marakna@rysun.com>
2026-06-11 21:35:55 +0530

    corsproxy applied

--- src/components/Body.js
+++ src/components/Body.js
@@ -14,7 +14,7 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.02760&lng=72.58710&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.02760&lng=72.58710&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
 
     const jsonData = await data.json();
